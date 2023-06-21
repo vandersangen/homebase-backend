cf349652a872c1eec20b5d76be88d69f4ec3f162
Lars van der Sangen <lars@vandersangen.dev>
2023-06-21 14:11:28 +0200

    Add introduction(text) endpoint + update frontend dev docker-compose definition

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -17,19 +17,31 @@ class ApiController extends AbstractController
             [
                 'name' => 'LinkedIn',
                 'href' => 'https://www.linkedin.com/in/lars-van-der-sangen-28114086/',
+                'target' => '_blank',
             ],
             [
                 'name' => 'GitHub',
                 'href' => 'https://github.com/vandersangen',
+                'target' => '_blank',
             ],
             [
                 'name' => 'Email',
                 'href' => 'mailto:lars@vandersangen.dev',
+                'target' => '_blank',
             ],
             [
                 'name' => 'WhatsApp',
                 'href' => 'https://wa.me/31611858342',
+                'target' => '_blank',
             ]
         ]);
+    }
+
+    #[Route('/api/introduction', name: 'app_api_introduction')]
+    public function introduction(): JsonResponse
+    {
+        return $this->json(
+            'Hallo, welkom! Kijk gerust even wat je nodig hebt hier 👨‍💻'
+        );
     }
 }
